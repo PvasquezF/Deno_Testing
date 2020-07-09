ce3eff21aed4588572d44c43070db2b5fbb4f1b7
Pavel Vasquez <alexanderpavelv32@gmail.com>
2020-07-08 23:33:31 -0600

    create product endpoint

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,6 +1,6 @@
 import { products } from '../products.ts';
 import { Product } from '../types.ts';
-
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 let getProducts = ({ response }: { response: any }) => {
     response.body = {
         success: true,
@@ -29,4 +29,29 @@ let getProduct = ({ params, response }: { params: { id: string }, response: any 
         }
     }
 }
-export { getProducts, getProduct };
+
+let addProduct = async ({ request, response }: { request: any, response: any }) => {
+    const body = await request.body();
+    if (!request.hasBody) {
+        response.status = 400;
+        response.body = {
+            success: false,
+            error: {
+                name: 'NoDataFound',
+                errors: [
+                    'No se ha encontrado el cuerpo de la petición'
+                ]
+            }
+        }
+    } else {
+        const product: Product = body.value;
+        product.id = v4.generate();
+        products.push(product);
+        response.status = 201;
+        response.body = {
+            success: false,
+            data: product
+        }
+    }
+}
+export { getProducts, getProduct, addProduct };
--- routes/products.ts
+++ routes/products.ts
@@ -1,8 +1,9 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
-import {getProducts, getProduct} from '../controllers/products.ts';
+import { getProducts, getProduct, addProduct } from '../controllers/products.ts';
 const productsRouter = new Router();
 productsRouter.get('/api/v1/products', getProducts)
-            .get('/api/v1/products/:id',getProduct)
+    .get('/api/v1/products/:id', getProduct)
+    .post('/api/v1/products', addProduct)
 
 export default productsRouter;
 
